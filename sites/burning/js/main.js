b4bccdb033ebb0b4c3b6b0d56f94f36fe811b6b9
ktotoad <138858148+ktotoad@users.noreply.github.com>
2023-07-07 10:48:44 +0300

    Add files via upload

---
+++ sites/burning/js/main.js
@@ -0,0 +1,39 @@
+
+function ibg(){
+		let ibg=document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if(ibg[i].querySelector('img')){
+			ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+		}
+	}
+}
+ibg();
+
+$('.wrapper').addClass('loaded');
+
+$(document).ready(function(){
+	$('.icon-menu').click(function(event) {
+		$(this).toggleClass('active');
+		$('.menu__body').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+//burger=====================================================================================================================================================
+$('.menu-header__icon').click(function(event) {
+	$(this).toggleClass('active');
+	$('.menu-header__menu').toggleClass('active');
+	if($(this).hasClass('active')){
+		$('body').data('scroll',$(window).scrollTop());
+	}
+		$('body').toggleClass('lock');
+	if(!$(this).hasClass('active')){
+		$('body,html').scrollTop(parseInt($('body').data('scroll')));
+	}
+});
+
+//Paralax=====================================================================================================================================================
+$(window).scroll(function(event) {
+		var s=0-$(this).scrollTop()/2;
+	$('.main__image').css('transform','translate3d(0,'+s+'px,0)');
+});
