7277a491ba8186e09d1e4eebb6451a540ea8b4f5
ktotoad <138858148+ktotoad@users.noreply.github.com>
2024-04-12 15:37:57 +0300

    Add files via upload

---
+++ sites/claster/js/main.js
@@ -0,0 +1,12 @@
+
+/*Loading================================================================================*/
+window.addEventListener('load', function () {
+    const loader = document.querySelector('.loader');
+    loader.classList.add('hidden');
+});
+
+/*Content_download================================================================================*/
+let wrapper = document.querySelector('.wrapper');
+window.addEventListener('load', (event) => {
+	wrapper.classList.add('loaded');
+});
