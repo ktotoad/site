ae0592f4f3220906be737ddce7bc7a06a19f327c
ktotoad <138858148+ktotoad@users.noreply.github.com>
2024-04-14 21:01:53 +0300

    Add files via upload

--- sites/claster/js/main.js
+++ sites/claster/js/main.js
@@ -10,3 +10,20 @@ let wrapper = document.querySelector('.wrapper');
 window.addEventListener('load', (event) => {
 	wrapper.classList.add('loaded');
 });
+
+//Вывод видео==================================================================================================================================================
+window.addEventListener('DOMContentLoaded', function() {
+	let broadcast = document.querySelectorAll('#broadcast');
+
+	for (let i=0; i < broadcast.length; i++) {
+		broadcast[i].addEventListener('click', function() {
+			let src = broadcast[i].dataset.src;
+
+			if (broadcast[i].classList.contains('ready')) {
+				return;
+			}
+			broadcast[i].classList.add('ready');
+			broadcast[i].insertAdjacentHTML('afterbegin', '<iframe src="' + src + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+		});
+	}
+});
