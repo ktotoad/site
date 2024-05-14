35827256368da9d057e7f2ec8753b1037e434bc4
ktotoad <138858148+ktotoad@users.noreply.github.com>
2024-05-14 08:30:48 +0300

    Add files via upload

---
+++ sites/tpi28/js/main.js
@@ -0,0 +1,57 @@
+
+/*Loading================================================================================*/
+window.addEventListener('load', function () {
+    const loader = document.querySelector('.loader');
+    loader.classList.add('hidden');
+});
+
+/*Content_download================================================================================*/
+let wrapper = document.querySelector('.wrapper');
+window.addEventListener('load', (event) => {
+	wrapper.classList.add('loaded');
+});
+
+//BuildSlider======================================================================================================================================================
+function buildSliders() {
+	let sliders = document.querySelectorAll('[class*="__swiper"]:not(.swiper-wrapper)');
+	if (sliders) {
+		sliders.forEach(slider => {
+			slider.parentElement.classList.add('swiper');
+			slider.classList.add('swiper-wrapper');
+			for(const slide of slider.children) {
+				slide.classList.add('swiper-slide');
+			}
+		});
+	}
+}
+
+//Инициализация_Swiper===============================================================================================================================================
+function initSliders() {
+	buildSliders();
+
+	if (document.querySelector('.stub-page__slider')) {
+		new Swiper('.stub-page__slider', {
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			parallax: true,
+			autoHeight: true,
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			speed: 800,
+			pagination: {
+		        el: ".stub-page__pagination",
+		        clickable: true,
+		    },
+		    navigation: {
+		        nextEl: ".stub-page__next",
+		        prevEl: ".stub-page__prev",
+		    },
+		});
+	}
+}
+
+initSliders();
