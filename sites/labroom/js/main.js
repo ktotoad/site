d360aff383077343ab5d186e70830caa5f13f0e2
ktotoad <138858148+ktotoad@users.noreply.github.com>
2024-09-24 15:45:08 +0300

    Add files via upload

---
+++ sites/labroom/js/main.js
@@ -0,0 +1,52 @@
+
+/*Content_download================================================================================*/
+let wrapper = document.querySelector('.wrapper');
+window.addEventListener('load', (event) => {
+	wrapper.classList.add('loaded');
+});
+
+//burger=====================================================================================================================================================
+if (document.querySelector('.icon-menu')) {
+	const iconSubmenu = document.querySelector('.icon-menu');
+	const submenuBody = document.querySelector('.submenu__body');
+	const body = document.querySelector('body');
+	let submenuItems = document.querySelectorAll('.submenu__nav_links a');
+
+	iconSubmenu.addEventListener('click', 
+		function clickButtonBurger(event) {
+			iconSubmenu.classList.toggle('active');
+			submenuBody.classList.toggle('active');
+			body.classList.toggle('lock');
+		});
+
+	submenuItems.forEach(submenuItem => {
+		submenuItem.addEventListener('click', 
+			function clickButtonBurger(event) {
+				iconSubmenu.classList.remove('active');
+				submenuBody.classList.remove('active');
+				body.classList.remove('lock');
+			});
+	});
+
+	$('.submenu__nav li').on('mouseenter',function(){
+		$(this).siblings().addClass('notactive');
+		$('.submenu__image[data-link="'+$(this).data('link')+'"]').addClass('active');
+	});
+	$('.submenu__nav li').on('mouseleave',function(){
+		$(this).siblings().removeClass('notactive');
+		$('.submenu__image[data-link="'+$(this).data('link')+'"]').removeClass('active');
+		if(!$('.submenu__image.active').length){
+			$('.submenu__image:first').addClass('active');
+		}
+	});
+}
+//InputMask===============================================================================================================================================
+function inputElements() {
+	let inputPhones = document.querySelectorAll("input[data-format]");
+	inputPhones.forEach(inputPhone => {
+		let phoneMask = new IMask(inputPhone, {
+			mask: inputPhone.getAttribute("data-format")
+		});
+	});
+}
+inputElements();
