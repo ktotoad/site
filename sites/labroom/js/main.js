9b4faa8fe9cdf20ddaf98408a8a3503cb1c086c0
ktotoad <138858148+ktotoad@users.noreply.github.com>
2024-09-30 22:11:09 +0300

    Add files via upload

--- sites/labroom/js/main.js
+++ sites/labroom/js/main.js
@@ -40,6 +40,42 @@ if (document.querySelector('.icon-menu')) {
 		}
 	});
 }
+const animItems = document.querySelectorAll('.anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll(params) {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight /animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+				animItem.classList.add('active');
+			} else {
+				if (!animItem.classList.contains('anim-no-hide')) {
+					animItem.classList.remove('active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+
+	setTimeout(() => {
+		animOnScroll();
+	}, 300);
+}
 //InputMask===============================================================================================================================================
 function inputElements() {
 	let inputPhones = document.querySelectorAll("input[data-format]");
@@ -347,3 +383,17 @@ function tabs() {
 	}
 }
 tabs();
+//slides_main==================================================================================================================================================
+if (document.querySelector('#slidesmain')) {
+	const slidesMain = document.querySelector('#slidesmain');
+	const slidesItems = slidesMain.querySelectorAll('#slidesitem');
+
+	slidesMain.addEventListener('click', e => {
+		if(e.target.closest('#slidesitem')) {
+			slidesItems.forEach( slidesItem => {
+				slidesItem.classList.remove('active');
+			});
+			e.target.closest('#slidesitem').classList.add('active');
+		}
+	});
+}
