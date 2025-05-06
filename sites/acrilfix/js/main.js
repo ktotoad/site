c0d96ceec9dd7687679618b2214e3b7f31b1bef8
ktotoad <138858148+ktotoad@users.noreply.github.com>
2025-05-06 15:24:31 +0300

    Add files via upload

---
+++ sites/acrilfix/js/main.js
@@ -0,0 +1,40 @@
+
+/*Content_download================================================================================*/
+let wrapper = document.querySelector('.wrapper');
+window.addEventListener('load', (event) => {
+	wrapper.classList.add('loaded');
+});
+
+//burger=====================================================================================================================================================
+const iconMenu = document.querySelector('.icon-menu');
+const menuBody = document.querySelector('.header__body');
+const body = document.querySelector('body');
+
+if (iconMenu) {
+	iconMenu.addEventListener('click', 
+		function clickButtonBurger(event) {
+			iconMenu.classList.toggle('active');
+			menuBody.classList.toggle('active');
+			body.classList.toggle('lock');
+		});
+}
+//InputMask===============================================================================================================================================
+function inputElements() {
+	let inputPhones = document.querySelectorAll("input[data-format]");
+	inputPhones.forEach(inputPhone => {
+		let phoneMask = new IMask(inputPhone, {
+			mask: inputPhone.getAttribute("data-format")
+		});
+	});
+}
+inputElements();
+//prices===================================================================================================================================
+if (document.querySelectorAll(".js_price")) {
+  function numberWithSpaces(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  }
+  let js_prices = document.querySelectorAll(".js_price");
+  js_prices.forEach((js_price) => {
+      js_price.textContent = numberWithSpaces(js_price.textContent);
+  })
+}
